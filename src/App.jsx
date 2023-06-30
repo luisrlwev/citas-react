56d95f23054f0730f5972712f084164ee3a4693d
Luis Angel Hernandez <rlwev@hotmail.com>
2023-06-30 16:48:23 -0500

    Aprendiendo componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
+import Formulario from "./components/Formulario"
+import ListadoPacientes from "./components/ListadoPacientes"
+import Header from "./components/header"
+
 function App() {
-
-  // Puedes crear funciones e ifs antes del return
-  const sumar = () => {
-    const numero = 5;
-
-    if(numero > 5){
-      console.log("Si es mayor a 5")
-    }else{
-      console.log("No es mayor a 5")
-    }
-  }
-
-  sumar();
-
-  const edad = 18
 
   return (
     <>
-      {edad >= 18 ? 'Eres mayor de edad' : 'No eres mayor de edad'}
-      <div>
-        <h1>{'Hola Mundo'.toUpperCase()}</h1>
-        <h2>Edad del usuario: {edad}</h2>
-        <img src="alguna imagen.jp" alt="Foto"/>
-        <input type="text" />
-        <p>Un parrafo</p>
-      </div>
-      <div>
-        <h1>Hola Mundo</h1>
-        <img src="alguna imagen.jp" alt="Foto"/>
-        <input type="text" />
-        <p>Un parrafo</p>
-      </div>
+      <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
     </>
   )
 }
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <h2>Listado de pacientes</h2>
+  )
+}
+
+export default ListadoPacientes
