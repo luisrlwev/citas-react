84498d110dfa8f2e6d1460d5ab226ec1ac138f6a
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-03 16:41:08 -0500

    Clase 64 se terminaron lo estilos css

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,31 @@
 const Formulario = () => {
   return (
-    <div>
-        <h2>Formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+        <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+        <p className="text-lg mt-5 text-center mb-10">Añade pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
+        <form action="" className="bg-white shadow-md rounded-md py-10 px-5 mb-10">
+          <div className="mb-5">
+            <label htmlFor="mascota" className="block text-grey-700 uppercase font-bold">Nombre Mascota</label>
+            <input type="text" id="mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre de la mascota" />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="propietario" className="block text-grey-700 uppercase font-bold">Nombre Propietario</label>
+            <input type="text" id="propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Nombre del propietario" />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="email" className="block text-grey-700 uppercase font-bold">Email</label>
+            <input type="email" id="email" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Email contacto propietario" />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="alta" className="block text-grey-700 uppercase font-bold">Alta</label>
+            <input type="date" id="alta" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+          </div>
+          <div className="mb-5">
+            <label htmlFor="sintomas" className="block text-grey-700 uppercase font-bold">Sintomas</label>
+            <textarea name="sintomas" id="sintomas" cols="30" rows="10" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" placeholder="Describe los sintomas"></textarea>
+          </div>
+          <input type="submit" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700 cursor-pointer transition-colors" value='Agregar paciente'/>
+        </form>
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,13 @@
+const Paciente = () => {
+  return (
+    <div className='m-3 bg-white shadow-md px-5 py-10 rounded-md'>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: <span className='font-normal normal-case'>Luna</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: <span className='font-normal normal-case'>Luis</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Email: <span className='font-normal normal-case'>hola@luisweb.me</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta: <span className='font-normal normal-case'>03 de julio del 2023</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: <span className='font-normal normal-case'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta vero eveniet ex ab consequuntur quis omnis ullam eaque. Ipsa, sapiente eius deleniti quas nesciunt sunt cumque consequuntur adipisci ratione atque.</span></p>
+    </div>
+  )
+}
+
+export default Paciente
