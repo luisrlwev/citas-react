8ca10845c56fec7b2339bbf76561435bfef1f371
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-05 17:55:58 -0500

    Avance clase 78

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,11 +1,14 @@
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+
+  const { nombre, propietario, email, fecha, sintomas } = paciente
+
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-md'>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: <span className='font-normal normal-case'>Luna</span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: <span className='font-normal normal-case'>Luis</span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Email: <span className='font-normal normal-case'>hola@luisweb.me</span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta: <span className='font-normal normal-case'>03 de julio del 2023</span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: <span className='font-normal normal-case'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta vero eveniet ex ab consequuntur quis omnis ullam eaque. Ipsa, sapiente eius deleniti quas nesciunt sunt cumque consequuntur adipisci ratione atque.</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: <span className='font-normal normal-case'>{nombre}</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Propietario: <span className='font-normal normal-case'>{propietario}</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Email: <span className='font-normal normal-case'>{email}</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta: <span className='font-normal normal-case'>{fecha}</span></p>
+        <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: <span className='font-normal normal-case'>{sintomas}</span></p>
     </div>
   )
 }
