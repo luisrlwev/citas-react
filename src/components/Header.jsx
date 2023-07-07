cd4d682624f8b8bcd4b003f857af31c00328c7bc
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-07 17:35:46 -0500

    Se agregó el espacio en el header que faltaba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento Pacientes Con GitHub<span className='text-indigo-600'>Veterinaria</span></h1>
+    <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>Seguimiento Pacientes Con GitHub <span className='text-indigo-600'>Veterinaria</span></h1>
   )
 }
 
