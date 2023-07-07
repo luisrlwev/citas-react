57497d29ca260f51255d922ad98a164cb69d11d6
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-07 17:23:20 -0500

    Se arreglo el problema en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
-import Header from "./components/header"
+import Header from "./components/Header"
 
 function App() {
 
